07512aee825b388f17a69ba985a52d3539c48786
Sachin8412 <skray1205@gmail.com>
2024-10-27 11:24:46 +0530

    done

---
+++ app.js
@@ -0,0 +1,45 @@
+let menu = document.querySelector('#menu-bar');
+let nav = document.querySelector('.nav');
+
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.taggle('active');
+}
+window.onclick  = ()=>{
+    menu.classList.remove('fa-times');
+    nav.classList.renove('active');
+}
+
+document.querySelector('#search-icon').onclick =() =>{
+    document.querySelector('#search-from').classList.toggle('active');
+}
+document.querySelector('#close').onclick = () =>{
+    document.querySelector('#search-from').classList.remove('active');
+}
+
+var swiper = new Swiper(".home-slider", {
+    spaceBetween:30,
+    centeredSlides:true,
+    autoplay:{
+      delay:7500,
+      disableOnInteraction:false,
+    
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop:true,
+  });
+  var swiper = new Swiper(".review-slider", {
+    spaceBetween:30,
+    centeredSlides:true,
+    autoplay:{
+      delay:7500,
+      disableOnInteraction:false,
+    
+    },
+  
+    loop:true,
+  });
